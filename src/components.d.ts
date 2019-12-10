be84356addfea2236c764116073a9ce08c214aa7
Duy Vu <dtv15@imperial.co.uk>
2019-12-10 17:30:01 +0000

    Implemented calendar header

--- src/components.d.ts
+++ src/components.d.ts
@@ -10,53 +10,31 @@ import { HTMLStencilElement, JSXBase } from '@stencil/core/internal';
 
 
 export namespace Components {
-  interface MyComponent {
-    /**
-    * The first name
-    */
-    'first': string;
-    /**
-    * The last name
-    */
-    'last': string;
-    /**
-    * The middle name
-    */
-    'middle': string;
+  interface DvCalendar {
+    'date': Date;
   }
 }
 
 declare global {
 
 
-  interface HTMLMyComponentElement extends Components.MyComponent, HTMLStencilElement {}
-  var HTMLMyComponentElement: {
-    prototype: HTMLMyComponentElement;
-    new (): HTMLMyComponentElement;
+  interface HTMLDvCalendarElement extends Components.DvCalendar, HTMLStencilElement {}
+  var HTMLDvCalendarElement: {
+    prototype: HTMLDvCalendarElement;
+    new (): HTMLDvCalendarElement;
   };
   interface HTMLElementTagNameMap {
-    'my-component': HTMLMyComponentElement;
+    'dv-calendar': HTMLDvCalendarElement;
   }
 }
 
 declare namespace LocalJSX {
-  interface MyComponent {
-    /**
-    * The first name
-    */
-    'first'?: string;
-    /**
-    * The last name
-    */
-    'last'?: string;
-    /**
-    * The middle name
-    */
-    'middle'?: string;
+  interface DvCalendar {
+    'date'?: Date;
   }
 
   interface IntrinsicElements {
-    'my-component': MyComponent;
+    'dv-calendar': DvCalendar;
   }
 }
 
@@ -66,7 +44,7 @@ export { LocalJSX as JSX };
 declare module "@stencil/core" {
   export namespace JSX {
     interface IntrinsicElements {
-      'my-component': LocalJSX.MyComponent & JSXBase.HTMLAttributes<HTMLMyComponentElement>;
+      'dv-calendar': LocalJSX.DvCalendar & JSXBase.HTMLAttributes<HTMLDvCalendarElement>;
     }
   }
 }
