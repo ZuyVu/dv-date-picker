c409a138929d450b49479fbc9d8ad3926aa33454
Duy Vu <dtv15@imperial.co.uk>
2019-12-11 22:58:40 +0000

    a chosen date is now reflected on the input value element

--- src/components.d.ts
+++ src/components.d.ts
@@ -12,7 +12,7 @@ import { HTMLStencilElement, JSXBase } from '@stencil/core/internal';
 export namespace Components {
   interface DvCalendar {
     'date': Date;
-    'onDateChange': Function;
+    'dateChanceCallback': Function;
   }
   interface DvDatePicker {
     'dateString': string;
@@ -43,7 +43,7 @@ declare global {
 declare namespace LocalJSX {
   interface DvCalendar {
     'date'?: Date;
-    'onDateChange'?: Function;
+    'dateChanceCallback'?: Function;
   }
   interface DvDatePicker {
     'dateString'?: string;
